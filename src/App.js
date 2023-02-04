8e7aae24b308a252d38a3eca60311efe09eb3dcf
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-04 16:51:51 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import React from 'react';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="">
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <About />
+      <Contact />
     </div>
   );
 }
