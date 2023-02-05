a889d3f668de9ec7ec14853f9c3eee059a5455c0
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-05 15:23:54 +0100

    link scroll on click to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 function Skills() {
+  const {theme} = useContext(ThemeContext);
   return (
-    <div>
-      
-    </div>
+    <section className={theme} id='skills'>
+      <h3>SKILLS</h3>
+      <div className='content-wrap'>
+        <p></p>
+        <div className='tech-stack-container'>
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
