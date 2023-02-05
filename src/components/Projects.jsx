29e9e6593db038effdd527dd05b2e9aa923430f2
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-05 22:08:57 +0100

    second project added to projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,15 +5,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faSquareJs, faHtml5, faCss3, faSass } from '@fortawesome/free-brands-svg-icons'
 import { IoLogoFirebase } from "react-icons/io5"
 import Project1Dark from '../assets/project-1-dark.png'
+import Project1Light from '../assets/project-1-light.png'
+import Project2Dark from '../assets/project-2-dark.png'
+import Project2Light from '../assets/project-2-light.png'
 
 function Projects() {
   const {theme} = useContext(ThemeContext);
   return (
     <section className={'projects ' + theme} id='projects'>
       <h3>PROJECTS</h3>
+
       <div className='project project-1'>
         <div className="col col1">
-          <img src={Project1Dark} alt="image of first project" />
+          {theme === 'dark' ? <img src={Project1Dark} alt="image of first project" /> : <img src={Project1Light} alt="image of first project" />}
         </div>
         <div className="col col2">
 
@@ -56,6 +60,49 @@ function Projects() {
               <li>Firebase Realtime Databse to store saved offers</li>
               <li>Category filter</li>
             </ul>
+          </div>
+          <div className='cta-buttons'>
+
+            <a target='_blank' href="https://marcelfronczak.github.io/job-offers-app/" className='cta-page'>Live Page</a>
+            <a target='_blank' href="https://github.com/MarcelFronczak/job-offers-app.git" className='cta-code'>Source Code</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='project project-2'>
+        <div className="col col1">
+          {theme === 'dark' ? <img src={Project2Dark} alt="image of second project" /> : <img src={Project2Light} alt="image of second project" />}
+        </div>
+        <div className="col col2">
+
+          <h4>Barbershop Website</h4>
+
+            <div className='tech-container'>
+              <div className='tech-stack-btn'>
+                  <div className='tech-stack-content'>
+                    <p>Javascript</p>
+                    <FontAwesomeIcon icon={faSquareJs} />
+                  </div>
+              </div>
+              <div className='tech-stack-btn'>
+                <div className='tech-stack-content'>
+                  <p>HTML</p>
+                  <FontAwesomeIcon icon={faHtml5} />
+                </div>
+              </div>
+              <div className='tech-stack-btn'>
+                <div className='tech-stack-content'>
+                  <p>CSS</p>
+                  <FontAwesomeIcon icon={faCss3} />
+                </div>
+              </div>
+            </div>
+
+          <div className="description">
+            <p>Barbershop’s Responsive and Interactive Landing Page.</p>
+          </div>
+          <div className="description">
+            <p>Website is accessible on every device and browser.</p>
           </div>
           <div className='cta-buttons'>
 
