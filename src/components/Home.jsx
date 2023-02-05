b1e754b6fe207c507fae50b95a30ccacb542a06d
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-05 01:46:56 +0100

    resume btn + hero styling added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 import '../styles/home.scss'
+import resume from '../assets/M-Fronczak-CV.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
   const {theme} = useContext(ThemeContext);
@@ -9,6 +12,12 @@ function Home() {
       <div>
         <h1>Hi, I'm<br></br><span>Marcel Fronczak</span></h1>
         <p>Junior Frontend Developer</p>
+        <div className='gradient-wrap'>
+          <a href={resume} download='M-Fronczak-CV.pdf' className='resume-btn'>
+            Resume
+            <FontAwesomeIcon icon={faFileArrowDown} className='file-icon'/>
+          </a>
+        </div>
       </div>
     </section>
   )
