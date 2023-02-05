3bbd40f7351d89fd7c1b46cac8edcf3725ffc292
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-05 19:07:56 +0100

    skills section: tech stack with icons added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 import '../styles/skills.scss'
+import { faReact, faSquareJs, faHtml5, faCss3, faGitAlt, faGithub, faFigma, faSass } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Skills() {
   const {theme} = useContext(ThemeContext);
@@ -9,9 +11,58 @@ function Skills() {
       <h3>SKILLS</h3>
       <div className='content-wrap'>
         <p>These are the main technologies I’ve been using over the last year.<br></br>In the near future I’m planning to learn <span className='bolder'>Typescript</span> and <span className='bolder'>Node.js</span>.</p>
-        <div className='tech-stack-container'>
-
-        </div>
+        <div className='flex-wrap'>
+          <div className='tech-stack-container'>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>React</p>
+                <FontAwesomeIcon icon={faReact} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>JS</p>
+                <FontAwesomeIcon icon={faSquareJs} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>HTML</p>
+                <FontAwesomeIcon icon={faHtml5} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>CSS</p>
+                <FontAwesomeIcon icon={faCss3} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>Sass</p>
+                <FontAwesomeIcon icon={faSass} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>Git</p>
+                <FontAwesomeIcon icon={faGitAlt} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>GitHub</p>
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+            </div>
+            <div className='tech-stack-btn'>
+              <div className='tech-stack-content'>
+                <p>Figma</p>
+                <FontAwesomeIcon icon={faFigma} />
+              </div>
+            </div>
+          </div>
+          </div>
       </div>
     </section>
   )
