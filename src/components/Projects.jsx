c7226b4f2a26a43d692051be5d4962bbb1c14544
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-07 23:13:54 +0100

    third project added; github cta added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 import '../styles/projects.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faSquareJs, faHtml5, faCss3, faSass } from '@fortawesome/free-brands-svg-icons'
+import { faReact, faSquareJs, faHtml5, faCss3, faSass, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { IoLogoFirebase } from "react-icons/io5"
 import Project1Dark from '../assets/project-1-dark.png'
 import Project1Light from '../assets/project-1-light.png'
 import Project2Dark from '../assets/project-2-dark.png'
 import Project2Light from '../assets/project-2-light.png'
+import Project3Dark from '../assets/project-3-dark.png'
+import Project3Light from '../assets/project-3-light.png'
 
 function Projects() {
   const {theme} = useContext(ThemeContext);
@@ -106,11 +108,57 @@ function Projects() {
           </div>
           <div className='cta-buttons'>
 
-            <a target='_blank' href="https://marcelfronczak.github.io/job-offers-app/" className='cta-page'>Live Page</a>
-            <a target='_blank' href="https://github.com/MarcelFronczak/job-offers-app.git" className='cta-code'>Source Code</a>
+            <a target='_blank' href="https://marcelfronczak.github.io/barbershop-website/" className='cta-page'>Live Page</a>
+            <a target='_blank' href="https://github.com/MarcelFronczak/barbershop-website.git" className='cta-code'>Source Code</a>
           </div>
         </div>
       </div>
+
+      <div className='project project-1'>
+        <div className="col col1">
+          {theme === 'dark' ? <img src={Project3Dark} alt="image of third project" /> : <img src={Project3Light} alt="image of third project" />}
+        </div>
+        <div className="col col2">
+
+          <h4>Portfolio Website</h4>
+
+            <div className='tech-container'>
+              <div className='tech-stack-btn'>
+                  <div className='tech-stack-content'>
+                    <p>React</p>
+                    <FontAwesomeIcon icon={faReact} />
+                  </div>
+              </div>
+              <div className='tech-stack-btn'>
+                <div className='tech-stack-content'>
+                  <p>Sass</p>
+                  <FontAwesomeIcon icon={faSass} />
+                </div>
+              </div>
+            </div>
+
+          <div className="description">
+            <p className='ul-label'>This is my personal portfolio website you are currently viewing.</p>
+          </div>
+          <div className="description">
+            <p className='ul-label'>Its main purpose is to introduce myself, however building it was a great opportunity to get more comfortable with React and learn Sass (SCSS).</p>
+
+          </div>
+          <div className='cta-buttons'>
+
+            <a target='_blank' href="https://marcelfronczak.github.io/portfolio/" className='cta-page'>Live Page</a>
+            <a target='_blank' href="https://github.com/MarcelFronczak/portfolio.git" className='cta-code'>Source Code</a>
+          </div>
+        </div>
+      </div>
+
+        <a target="_blank" href="https://github.com/MarcelFronczak?tab=repositories" className="btn-cta-github">
+          <div className="btn-cta-github-content">
+            <p>See more on GitHub</p>
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+        </a>
+
     </section>
   )
 }
